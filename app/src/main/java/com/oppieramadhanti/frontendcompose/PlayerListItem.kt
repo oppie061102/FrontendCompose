5ce784c8d703b5ff43a19c102e5296f7d0d75b9c
Oppie Ramadhanti <oppieramadhanti@gmail.com>
2024-05-11 00:21:35 +0700

    feat: adding playerlistitem for display player

---
+++ app/src/main/java/com/oppieramadhanti/frontendcompose/PlayerListItem.kt
@@ -0,0 +1,62 @@
+package com.oppieramadhanti.frontendcompose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.oppieramadhanti.frontendcompose.data.model.Player
+
+@Composable
+fun PlayerListItem(player: Player) {
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 8.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp))
+    ) {
+        Row(Modifier.clickable {
+
+        }) {
+
+            PlayerImage(player)
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(text = player.name)
+                Text(text = player.position)
+            }
+        }
+    }
+}
+
+
+@Composable
+private fun PlayerImage(player: Player) {
+    Image(
+        painter = painterResource(id = player.playerImage),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .padding(8.dp)
+            .size(84.dp)
+            .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
+    )
+}
